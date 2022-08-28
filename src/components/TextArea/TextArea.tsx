8f61110d4b26cff2ca895a398123252ed1b384da
Jussi Nevavuori <jussi@nevavuori.fi>
2022-08-28 21:34:05 +0300

    Added textarea and feedback

---
+++ src/components/TextArea/TextArea.tsx
@@ -0,0 +1,39 @@
+import { c } from "@/utils/generic/classnames";
+import React from "react";
+import { ExtensionInputBaseProps, InputBase } from "../InputBase/InputBase";
+
+
+export type TextAreaProps =
+	React.TextareaHTMLAttributes<HTMLTextAreaElement>
+	& ExtensionInputBaseProps
+	& {
+		helperText?: string;
+	}
+
+export const TextArea = React.forwardRef<HTMLInputElement, TextAreaProps>((props, ref) => {
+	const { helperText, ...htmlProps } = InputBase.removeExtensionInputBaseProps(props)
+
+	return <InputBase {...props}>
+
+		<textarea
+			{...htmlProps}
+			className={c("bg-transparent py-2 text-black dark:text-white outline-none",
+				"placeholder:text-black-disabled dark:placeholder:text-white-disabled w-full",
+				c.variant(props.variant ?? "default")({
+					bordered: "px-2",
+					default: "px-2",
+					underlined: "px-0"
+				}))}
+		/>
+
+		{
+			helperText && <span className={c(
+				"absolute top-[100%] pt-1 text-sm",
+				c.if(props.error)("text-danger").else("text-black-disabled dark:text-white-disabled")
+			)}>
+				{helperText}
+			</span>
+		}
+
+	</InputBase>
+})
--- src/pages/app/settings/feedback.tsx
+++ src/pages/app/settings/feedback.tsx
@@ -1,10 +1,54 @@
+import { Button } from "@/components/Button/Button";
+import { TextArea } from "@/components/TextArea/TextArea";
+import { useNotify } from "@/features/Notifications/hooks/useNotify";
 import { AppLayout } from "@/layouts/app/AppLayout";
 import { SettingsLayout } from "@/layouts/settings/SettingsLayout";
+import { trpc } from "@/utils/trpc";
+import { useState } from "react";
 
 export default function FeedbackSettingsPage() {
+	const notify = useNotify();
+	const [message, setMessage] = useState("");
+	const mutation = trpc.useMutation(["feedback.send"], {
+		onSuccess: () => {
+			setMessage("")
+			notify.success("Succesfully sent message")
+		}
+	})
+
 	return <AppLayout active="settings">
 		<SettingsLayout title="Send Feedback">
-			Feedback
+			<section className="flex flex-col py-4">
+				<p>
+					Contact us
+				</p>
+
+				<div className="p-2" />
+
+				<p className="text-sm text-black-secondary dark:text-white-secondary max-w-md">
+					Please contact us and leave feedback about the application, any
+					bugs or features you would like to see.
+				</p>
+
+				<div className="p-4" />
+
+				<TextArea
+					value={message}
+					onChange={e => setMessage(e.target.value)}
+					fullWidth
+					disabled={mutation.isLoading}
+					rows={5}
+				/>
+
+				<div className="p-4" />
+
+				<Button
+					onClick={() => { if (message.trim()) mutation.mutate({ message }) }}
+					loading={mutation.isLoading}
+				>
+					Send
+				</Button>
+			</section>
 		</SettingsLayout>
 	</AppLayout>
 }
