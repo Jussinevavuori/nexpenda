f41701945c2505b77054dafb9b9dee8b6bc082f0
Jussi Nevavuori <jussi@nevavuori.fi>
2022-10-09 12:29:23 +0300

    Updated messages for migration

--- src/pages/app/welcome/index.tsx
+++ src/pages/app/welcome/index.tsx
@@ -16,72 +16,42 @@ export default function WelcomePage() {
 	const [message, setMessage] = useState("Setting you up...");
 	const [isMigrated, setIsMigrated] = useState(false);
 
-	const userdataMutation = trpc.useMutation("migrations.migrate.userdata", {
-		onMutate: () => setMessage("Fetching your user data...")
-	})
-	const cleardataMutation = trpc.useMutation("migrations.migrate.cleardata", {
-		onMutate: () => setMessage("Clearing your current data...")
-	})
-	const getOldDataMutation = trpc.useMutation("migrations.migrate.getOldData", {
-		onMutate: () => setMessage("Fetching your old data...")
-	})
-	const pushCategoriesMutation = trpc.useMutation("migrations.migrate.pushCategories", {
-		onMutate: () => setMessage("Updating your categories...")
-	})
-	const pushSchedulesMutation = trpc.useMutation("migrations.migrate.pushSchedules", {
-		onMutate: () => setMessage("Updating your transaction schedules...")
-	})
-	const pushTransactionsMutation = trpc.useMutation("migrations.migrate.pushTransactions", {
-		onMutate: () => setMessage("Updating your transactions...")
-	})
+	const userdataMutation = trpc.useMutation("migrations.migrate.userdata")
+	const cleardataMutation = trpc.useMutation("migrations.migrate.cleardata")
+	const getOldDataMutation = trpc.useMutation("migrations.migrate.getOldData")
+	const pushCategoriesMutation = trpc.useMutation("migrations.migrate.pushCategories")
+	const pushSchedulesMutation = trpc.useMutation("migrations.migrate.pushSchedules")
+	const pushTransactionsMutation = trpc.useMutation("migrations.migrate.pushTransactions")
+
 	const migrationMutation = useMutation(async () => {
-		if (window.location.href.includes("localhost")) await cleardataMutation.mutateAsync({});
+		if (window.location.href.includes("localhost")) {
+			setMessage("Clearing your old data...")
+			await cleardataMutation.mutateAsync({});
+		}
+
+		setMessage("Looking up previous data...")
 		const user = await userdataMutation.mutateAsync({});
-		ProgressBarPubSub.publish({ key: "welcome", value: 0.5, target: 3 })
+		ProgressBarPubSub.publish({ key: "welcome", value: 1, target: 5 })
+
+		setMessage("Listing all things to be copied...")
 		const data = await getOldDataMutation.mutateAsync({ oldUserId: user.id })
-		ProgressBarPubSub.publish({ key: "welcome", value: 1, target: 3 })
+		ProgressBarPubSub.publish({ key: "welcome", value: 2, target: 5 })
 
 		const ctChunks = chunkify(data.oldCategories, 50);
 		const scChunks = chunkify(data.oldSchedules, 50);
 		const txChunks = chunkify(data.oldTransactions, 50);
 
-		const itemsToPost = data.oldCategories.length + data.oldSchedules.length + data.oldTransactions.length;
-		let itemsPosted = 0;
+		setMessage(`Copying ${data.oldCategories.length} categories`)
+		await Promise.allSettled(ctChunks.map(categories => pushCategoriesMutation.mutateAsync({ categories })))
+		ProgressBarPubSub.publish({ key: "welcome", value: 3, target: 5 })
 
-		const update = (items: number) => {
-			itemsPosted += items;
-			ProgressBarPubSub.publish({
-				key: "welcome",
-				value: 1 + 2 * (itemsPosted / itemsToPost),
-				target: 3,
-			})
-		}
+		setMessage(`Copying ${data.oldSchedules.length} transaction schedules`)
+		await Promise.allSettled(scChunks.map(schedules => pushSchedulesMutation.mutateAsync({ schedules })))
+		ProgressBarPubSub.publish({ key: "welcome", value: 4, target: 5 })
 
-		console.log({ ctChunks, scChunks, txChunks })
-
-		await Promise.allSettled(
-			ctChunks.map(categories =>
-				pushCategoriesMutation
-					.mutateAsync({ categories })
-					.then(() => update(categories.length))
-			)
-		)
-
-		await Promise.allSettled(
-			scChunks.map(schedules =>
-				pushSchedulesMutation
-					.mutateAsync({ schedules })
-					.then(() => update(schedules.length))
-			)
-		)
-
-		await Promise.allSettled(
-			txChunks.map(transactions =>
-				pushTransactionsMutation
-					.mutateAsync({ transactions })
-					.then(() => update(transactions.length))
-			)
-		)
+		setMessage(`Copying ${data.oldTransactions.length} transactions`)
+		await Promise.allSettled(txChunks.map(transactions => pushTransactionsMutation.mutateAsync({ transactions })))
+		ProgressBarPubSub.publish({ key: "welcome", value: 5, target: 5 })
 
 	}, {
 		onSettled() {
