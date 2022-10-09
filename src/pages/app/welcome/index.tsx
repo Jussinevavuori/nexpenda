6fbbfc179ac9594102a2284112aa2c165bf903d8
Jussi Nevavuori <jussi@nevavuori.fi>
2022-10-09 12:01:53 +0300

    Chunkify welcome actions

--- src/pages/app/welcome/index.tsx
+++ src/pages/app/welcome/index.tsx
@@ -1,49 +1,79 @@
 import { Button } from "@/components/Button/Button";
-import { LoadingSpinner } from "@/components/LoadingSpinner/LoadingSpinner";
 import { PageHead } from "@/components/PageHead/PageHead";
+import { ProgressBar } from "@/components/ProgressBar/ProgressBar";
+import { ProgressBarPubSub } from "@/components/ProgressBar/utils/ProgressBarPubSub";
 import { useEffectOnce } from "@/hooks/useEffectOnce";
 import { useRequireAuth } from "@/hooks/useRequireAuth";
 import { useTimeSinceMount } from "@/hooks/useTimeSinceMount";
+import { chunkify } from "@/utils/generic/chunkify";
 import { pages } from "@/utils/pages";
 import { trpc } from "@/utils/trpc";
 import { useState } from "react";
 import { useMutation } from "react-query";
 
-const messages = [
-	"Setting you up...",
-	"Doing things...",
-	"Juuust a minute",
-	"Shouldn't be much more...",
-	"We're almost there...",
-	"Just hang on!",
-	"Not much longer",
-	"Aaalmost...",
-]
-
 export default function WelcomePage() {
 	useRequireAuth();
 
 	const time = useTimeSinceMount();
-	const message = messages[Math.min(messages.length - 1, Math.floor(time / 2500))]
 
+	const [message, setMessage] = useState("Setting you up...");
 	const [isMigrated, setIsMigrated] = useState(false);
 
-	const userdataMutation = trpc.useMutation("migrations.migrate.userdata")
-	const cleardataMutation = trpc.useMutation("migrations.migrate.cleardata")
-	const getOldDataMutation = trpc.useMutation("migrations.migrate.getOldData")
-	const pushCategoriesMutation = trpc.useMutation("migrations.migrate.pushCategories")
-	const pushSchedulesMutation = trpc.useMutation("migrations.migrate.pushSchedules")
-	const pushTransactionsMutation = trpc.useMutation("migrations.migrate.pushTransactions")
+	const userdataMutation = trpc.useMutation("migrations.migrate.userdata", {
+		onMutate: () => setMessage("Fetching your user data...")
+	})
+	const cleardataMutation = trpc.useMutation("migrations.migrate.cleardata", {
+		onMutate: () => setMessage("Clearing your current data...")
+	})
+	const getOldDataMutation = trpc.useMutation("migrations.migrate.getOldData", {
+		onMutate: () => setMessage("Fetching your old data...")
+	})
+	const pushCategoriesMutation = trpc.useMutation("migrations.migrate.pushCategories", {
+		onMutate: () => setMessage("Updating your categories...")
+	})
+	const pushSchedulesMutation = trpc.useMutation("migrations.migrate.pushSchedules", {
+		onMutate: () => setMessage("Updating your transaction schedules...")
+	})
+	const pushTransactionsMutation = trpc.useMutation("migrations.migrate.pushTransactions", {
+		onMutate: () => setMessage("Updating your transactions...")
+	})
 	const migrationMutation = useMutation(async () => {
-		if (window.location.href.includes("localhost")) {
-			console.log("Clearing data")
-			await cleardataMutation.mutateAsync({});
-		}
+		if (window.location.href.includes("localhost")) await cleardataMutation.mutateAsync({});
 		const user = await userdataMutation.mutateAsync({});
+		ProgressBarPubSub.publish({ key: "welcome", value: 1, target: 6 })
 		const data = await getOldDataMutation.mutateAsync({ oldUserId: user.id })
-		await pushCategoriesMutation.mutateAsync({ categories: data.oldCategories })
-		await pushSchedulesMutation.mutateAsync({ schedules: data.oldSchedules })
-		await pushTransactionsMutation.mutateAsync({ transactions: data.oldTransactions })
+		ProgressBarPubSub.publish({ key: "welcome", value: 2, target: 6 })
+
+		const ctChunks = chunkify(data.oldCategories, 50);
+		const scChunks = chunkify(data.oldSchedules, 50);
+		const txChunks = chunkify(data.oldTransactions, 50);
+
+		const itemsToPost = data.oldCategories.length + data.oldSchedules.length + data.oldTransactions.length;
+		let itemsPosted = 0;
+
+		const update = (items: number) => {
+			itemsPosted += items;
+			ProgressBarPubSub.publish({
+				key: "welcome",
+				value: itemsPosted + itemsToPost,
+				target: 3 * itemsToPost,
+			})
+		}
+
+		for (const chunk of ctChunks) {
+			await pushCategoriesMutation.mutateAsync({ categories: chunk });
+			update(chunk.length);
+		}
+
+		for (const chunk of scChunks) {
+			await pushSchedulesMutation.mutateAsync({ schedules: chunk });
+			update(chunk.length);
+		}
+
+		for (const chunk of txChunks) {
+			await pushTransactionsMutation.mutateAsync({ transactions: chunk });
+			update(chunk.length);
+		}
 	}, {
 		onSettled() {
 			setIsMigrated(true);
@@ -77,11 +107,15 @@ export default function WelcomePage() {
 				isMigrated ? <Button.Link href={pages.onboarding.whatnext}>
 					Continue
 				</Button.Link> : <>
-					<LoadingSpinner color="primary-500" />
+					<ProgressBar progress="welcome" />
 					<div>{message}</div>
 				</>
 			}
 		</div>
 
 	</div>
+}
+
+function useProgressBarValue(arg0: string) {
+	throw new Error("Function not implemented.");
 }
